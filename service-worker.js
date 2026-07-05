e93ad57df071d74cc1574e7e83a0a0a5db712b15
juanluisalvarezriestra <riestracovian@gmail.com>
2026-07-05 11:30:59 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-const CACHE_NAME='diario-trading-pwa-v2';const URLS=['./','./index.html','./manifest.json','./service-worker.js','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+
